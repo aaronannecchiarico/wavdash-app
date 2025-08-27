6cd7e8d42a7dac91806337ff786491c8560b65fe
aannecchiarico <aannecchiarico@protonmail.com>
2025-08-26 20:29:40 -0400

    theme

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx', 'vendor/resma/filament-awin-theme/resources/css/theme.css'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
