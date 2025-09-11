115f268bccc3316237db825f9b3f34f7fb562ad6
aannecchiarico <aannecchiarico@protonmail.com>
2025-09-10 20:58:46 -0400

    fix tests due to ssr

--- app/config/inertia.php
+++ app/config/inertia.php
@@ -16,7 +16,7 @@ return [
     */
 
     'ssr' => [
-        'enabled' => true,
+        'enabled' => env('APP_ENV') !== 'testing',
         'url' => 'http://127.0.0.1:13714',
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
 
